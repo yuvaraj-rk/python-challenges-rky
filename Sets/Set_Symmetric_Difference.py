5ab39dd7f9fae725ffcd90c12856a515dd4ae843
yuvaraj-rk <yuvarajrk2007@gmail.com>
2026-03-24 13:57:26 +0100

    Solved: HackerRank - Sets Module -> Symmetric Difference Operation challenges

---
+++ Sets/Set_Symmetric_Difference.py
@@ -0,0 +1,9 @@
+#https://www.hackerrank.com/challenges/py-set-intersection-operation/problem?isFullScreen=true
+
+a = input()
+english = set(map(int, input().split()))
+
+b = input()
+french = set(map(int, input().split()))
+
+print(len(english.intersection(french)))
