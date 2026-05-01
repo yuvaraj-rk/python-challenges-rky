878b1aeaa56aa0ef2b996dfddd05c85b1116d2bd
yuvaraj-rk <yuvarajrk2007@gmail.com>
2026-05-01 12:30:32 +0200

    Solved: HackerRank - NumPy Module -> Eye and Identity Challenge

---
+++ Numpy/Eye_and_Identity.py
@@ -0,0 +1,9 @@
+#https://www.hackerrank.com/challenges/np-eye-and-identity/problem?isFullScreen=true
+
+import numpy
+
+numpy.set_printoptions(legacy='1.13')
+
+N, M = map(int, input().split())
+
+print(numpy.eye(N, M))
